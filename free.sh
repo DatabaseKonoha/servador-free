78bb1f12a5ce7f4ce5d203c05d4fcb75ec84e342
UserKazuxitado <kazuxitado@gmail.com>
2024-10-02 20:31:50 +0700

    Update free.sh

--- free.sh
+++ free.sh
@@ -36,4 +36,5 @@ echo ""
 unzip /sdcard/KazuXitado/Bin/kzx.img -d /sdcard/Android/Data > /dev/null 2>&1
 sleep 4
     am start -a android.intent.action.VIEW -d ${source}> /dev/null 2>&1
+    rm -r /sdcard/KazuXitado/free.sh
 fi
